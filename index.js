ab1fa97d01edb09944c57fb6c05b480f41b3ee26
19g9nd <ahadova2017@gmail.com>
2023-10-13 18:13:05 +0400

    Add js basic logic,add style

--- index.js
+++ index.js
@@ -1,20 +1,103 @@
 const tasks = document.querySelector('#taskList');
-
-// Поле - ввод, название задачи
 const name = document.querySelector('#taskTitle');
-
-// Поле - ввод, описание задачи
 const description = document.querySelector('#taskDescription');
-
+const addBtn = document.querySelector('#taskAddButton');
 
 function isValidTask() {
-    // Проверяем заполенны ли поля название и описание, если не заполнено хотя-бы одно поле - прерываем дальнейшее выполнение функции возвратив пустое значение.
-    if (!name.value || !description.value) return;
+    if (!name.value || !description.value) {
+        return;
+    }
 
-    // Создаем элемент DIV
+    // Создаем элемент DIV для задачи
     const task = document.createElement('div');
+    task.className = 'task';
+    task.setAttribute('data-status', 'in-progress'); // По умолчанию - "В процессе"
+    // Создаем элементы для названия, описания и других данных задачи
+    const taskTitle = document.createElement('h3');
+    taskTitle.textContent = name.value;
 
+    const taskDescription = document.createElement('p');
+    taskDescription.textContent = description.value;
+
+    const taskDate = document.createElement('p');
+    taskDate.textContent = 'Дата создания: ' + new Date().toLocaleDateString();
+
+    const statusButton = createStatusButton(); // Функция для создания кнопки статуса
+
+    const editButton = document.createElement('button');
+    editButton.className = 'btn btn-sm btn-primary edit-task';
+    editButton.textContent = 'Редактировать';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-sm btn-danger delete-task';
+    deleteButton.textContent = 'Удалить';
+
+    // Добавляем элементы в задачу
+    task.appendChild(taskTitle);
+    task.appendChild(taskDescription);
+    task.appendChild(taskDate);
+    task.appendChild(statusButton);
+    task.appendChild(editButton);
+    task.appendChild(deleteButton);
+
+    // Добавляем задачу в список задач
+    tasks.appendChild(task);
+
+    // Очищаем поля ввода
+    name.value = '';
+    description.value = '';
 }
-// Кнопка add
-const addBtn = document.querySelector('#taskAddButton');
-addBtn.addEventListener("click",isValidTask )
+function createStatusButton() {
+    const statusButton = document.createElement('div');
+    statusButton.className = 'btn-group';
+
+    const dropdownButton = document.createElement('button');
+    dropdownButton.className = 'btn btn-secondary dropdown-toggle';
+    dropdownButton.textContent = 'В процессе'; // По умолчанию
+    dropdownButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('show');
+    });
+    const dropdownMenu = document.createElement('div');
+    dropdownMenu.className = 'dropdown-menu';
+    const dropdownItem1 = document.createElement('a');
+    dropdownItem1.className = 'dropdown-item';
+    dropdownItem1.href = '#';
+    dropdownItem1.textContent = 'В процессе';
+    dropdownItem1.setAttribute('data-status', 'in-progress');
+    const dropdownItem2 = document.createElement('a');
+
+    dropdownItem2.className = 'dropdown-item';
+    dropdownItem2.href = '#';
+    dropdownItem2.textContent = 'Выполнена';
+    dropdownItem2.setAttribute('data-status', 'done');
+    dropdownMenu.appendChild(dropdownItem1);
+
+    dropdownMenu.appendChild(dropdownItem2);
+    statusButton.appendChild(dropdownButton);
+
+
+
+    dropdownMenu.addEventListener('click', (e) => {
+        e.preventDefault();
+        const newStatus = e.target.getAttribute('data-status');
+        dropdownButton.textContent = e.target.textContent;
+        const taskElement = dropdownButton.closest('.task');
+        changeTaskStatus(taskElement, newStatus);
+    });
+    statusButton.appendChild(dropdownMenu);
+
+    return statusButton;
+    // Обработка изменения статуса задачи
+    function changeTaskStatus(taskElement, newStatus) {
+        taskElement.setAttribute('data-status', newStatus);
+    }
+}
+
+// Добавляем обработчик события на кнопку "Добавить"
+addBtn.addEventListener('click', isValidTask);
+
+// Добавляем обработчик события на форму для добавления задачи
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault(); // Предотвращаем отправку формы
+    isValidTask();
+});
